bcc9b82a0a0b5d4a1829d61c2c245b75ae2e58f6
Vanya <vanyavasylyshyn@gmail.com>
2020-04-09 10:30:50 +0300

    Fix sign up and null: false for shouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_062708) do
+ActiveRecord::Schema.define(version: 2020_04_09_072856) do
 
   create_table "shouts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
   end
