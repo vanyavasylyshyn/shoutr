71d800ba6fd8007f73ced9e243a08fda75173c23
Vanya <vanyavasylyshyn@gmail.com>
2020-04-09 09:21:38 +0300

    Added user and shout page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < Clearance::UsersController
+  def show
+    @user = User.find(params[:id])
+    @shouts = @user.shouts
+  end
 
   private
 
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,4 +3,8 @@ class Shout < ApplicationRecord
 
   validates :body, presence: true, length: { maximum: 144, minimum: 1 }
   validates :user, presence: true
+
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
